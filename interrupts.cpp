a22ee2c27241f1ee65bcd65dd4143c67ef089d75
Takuya ASADA <syuu@dokukino.com>
2012-08-21 03:46:33 +0900

    rename interrupts log

--- interrupts.cpp
+++ interrupts.cpp
@@ -18,11 +18,7 @@ void init_interrupts_logger(variables_map& vm,
 	string header;
 	ifstream interrupts("/proc/interrupts");
 	string line;
-	vector<string> names;
 	getline(interrupts, line);
-	trim_all(line);
-	split(names, line, is_any_of(" "));
-	names.erase(names.begin(), names.begin());
 
 	while(getline(interrupts, line)) {
 		vector<string> cols;
@@ -36,13 +32,13 @@ void init_interrupts_logger(variables_map& vm,
 	shared_ptr<global_stats_logger> logger(
 		new global_stats_logger(
 			"/proc/interrupts",
-			output + "/interrupts_%s.log",
+			output + "/interrupts%d.log",
 			1,
 			nintrs,
 			1,
 			ncpus,
 			header,
-			names,
+			EMPTY_VECTOR,
 			true,
 			PF_DIRECTION_COL
 		));
