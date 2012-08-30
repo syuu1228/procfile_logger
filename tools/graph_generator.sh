29257556f2bfba65b6140d471a92b46f934b9df5
Takuya ASADA <syuu@dokukino.com>
2012-08-30 14:08:08 +0900

    graph generator

---
+++ tools/graph_generator.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+BASEDIR=$1
+TARGETS=$2
+TARGETS_ARRAY=`echo $2|sed -e "s/,/ /g"`
+
+if [ "$BASEDIR" = "" -o "$TARGETS" = "" ];then
+	echo "graph_generator.sh [basedir] [targets]"
+	exit 1
+fi
+
+get_csv_array()
+{
+	NAME=$1
+	for i in $TARGETS_ARRAY
+	do
+		echo $BASEDIR/$i/$NAME.csv
+	done
+}
+
+get_csv_names()
+{
+	NAME=$1
+	ARRAY=`get_csv_array $NAME`
+	echo $ARRAY|sed -e "s/ /,/g"
+}
+
+graph_bar()
+{
+	NAME=$1
+	ruby graph_bar.rb $NAME $BASEDIR/$NAME `get_csv_names $NAME` $TARGETS
+}
+
+graph_line()
+{
+	NAME=$1
+	ruby graph_line.rb $NAME $BASEDIR/$NAME `get_csv_names $NAME` $TARGETS
+}
+
+NCPUS=`cat /proc/cpuinfo |grep processor|wc -l`
+NDOMAINS=`grep domain /proc/schedstat |awk '{print $1}'|sort|uniq|wc -l`
+
+for i in `seq 0 $(($NDOMAINS - 1))`; do
+	graph_line merged_interrupts$i
+	graph_line schedstat_cpu$i
+	for j in `seq 0 $(($NDOMAINS - 1))`; do
+		graph_line schedstat_cpu$i-domain$j
+	done
+done
+graph_line sum_merged_interrupts
+graph_line bias_merged_interrupts
+graph_bar sumall_merged_interrupts
+graph_bar biasall_merged_interrupts
+graph_line netstat_ipext
+graph_line netstat_tcpext
+graph_bar sumall_netstat_ipext
+graph_bar sumall_netstat_tcpext
+graph_line proc_sched_a
+graph_line proc_sched_b
+graph_line proc_sched_c
+graph_bar sumall_proc_sched_a
+graph_bar sumall_proc_sched_b
+graph_bar sumall_proc_sched_c
+graph_line sum_schedstat_cpu
+graph_line bias_schedstat_cpu
+graph_bar sumall_schedstat_cpu
+graph_bar biasall_schedstat_cpu
+graph_line sum_schedstat_domain
+graph_line bias_schedstat_domain
+graph_bar sumall_schedstat_domain
+graph_bar biasall_schedstat_domain
+for i in `seq 0 $(($NDOMAINS - 1))`; do
+	graph_line sum_schedstat_domain$i
+	graph_line bias_schedstat_domain$i
+	graph_bar sumall_schedstat_domain$i
+	graph_bar biasall_schedstat_domain$i
+done
+graph_line sum_stat
+graph_line bias_stat
+graph_bar sumall_stat
+graph_bar biasall_stat
